229cde82c13dbc5afac6715ffff6b9c506d93a13
otreblan <otreblain@gmail.com>
2021-06-28 09:36:15 -0500

    Add pkg-config file

--- cmake/FindAio.cmake
+++ cmake/FindAio.cmake
@@ -12,6 +12,6 @@ endif()
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(aio DEFAULT_MSG AIO_LIBRARIES AIO_INCLUDE_DIR)
+find_package_handle_standard_args(Aio DEFAULT_MSG AIO_LIBRARIES AIO_INCLUDE_DIR)
 
 mark_as_advanced(AIO_INCLUDE_DIR AIO_LIBRARIES)
