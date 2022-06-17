e6c50e4c0081448f0834eb6adb0d7e944e322f31
Slobodan Pavlic <slobodan.pavlic@gmail.com>
2022-06-17 18:49:58 -0400

    Updated Copyright year

--- include/rapidobj/rapidobj.hpp
+++ include/rapidobj/rapidobj.hpp
@@ -4,7 +4,7 @@ rapidobj - Fast Wavefront .obj file loader
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>
 SPDX-License-Identifier: MIT
-Copyright (c) 2021 Slobodan Pavlic
+Copyright (c) 2022 Slobodan Pavlic
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
